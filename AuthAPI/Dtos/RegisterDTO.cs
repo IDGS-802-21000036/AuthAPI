2c9a43645815aa836c7f7113e25e251f32f046de
Daniel Alejandro Lopez Garcia <danyalexlg@gmail.com>
2024-08-05 22:52:05 -0600

    Agregar archivos de proyecto.

---
+++ AuthAPI/Dtos/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthAPI.Dtos
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+        public string Password { get; set; }
+
+        public List<String>? Roles { get; set; }
+
+    }
+}
